156e2036e8b1f24c505bbff4bf457bc7f6b7d145
Hov <asatryanhovhannes1995@gmail.com>
2025-03-24 19:42:15 +0100

    added main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-// import Link from "next/link";
+import { Montserrat } from "next/font/google"; // Импортируем шрифт
 import NavBar from "@/components/NavBar";
 import VantaBackground from "@/components/VantaBackground";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "./globals.css";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
   subsets: ["latin"],
+  weight: ["400", "600", "700"], // Указываем нужные начертания
+  display: "swap", // Улучшает производительность загрузки шрифта
 });
 
 export const metadata = {
@@ -21,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body suppressHydrationWarning={true} className={montserrat.className}>
         <VantaBackground />
         <NavBar />
         <main>{children}</main>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import VantaBackground from "@/components/VantaBackground"; // Adjust the path if necessary
 
 import "@/app/globals.css";
-
+import styles from "@/app/page.module.css";
 export default function Home() {
   return (
     <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Asatryan Hovhannes</h1>
+        <p className={styles.description}>
+          Communications engineer
+          <br />
+          WEB developer
+        </p>
+      </div>
       {/* <VantaBackground /> */}
-      <div
+      {/* <div
         style={{
           position: "relative",
           zIndex: 1,
@@ -15,7 +23,7 @@ export default function Home() {
         }}>
         <h1>Welcome to My Next.js App</h1>
         <p>This app has a cool Vanta Waves background!</p>
-      </div>
+      </div> */}
     </>
   );
 }
